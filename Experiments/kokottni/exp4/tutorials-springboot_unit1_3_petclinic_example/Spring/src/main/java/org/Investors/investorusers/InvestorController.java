568f6cebc860c3898a66b4c3d1754bf4b9217eab
Nick Kokott <kokottni@iastate.edu>
2023-09-07 17:31:54 -0500

    Adding investors into exp3 that are functional

--- Experiments/kokottni/exp4/tutorials-springboot_unit1_3_petclinic_example/Spring/src/main/java/org/Investors/investorusers/InvestorController.java
+++ Experiments/kokottni/exp4/tutorials-springboot_unit1_3_petclinic_example/Spring/src/main/java/org/Investors/investorusers/InvestorController.java
@@ -1,4 +1,67 @@
 package org.Investors.investorusers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 public class InvestorController {
+
+    @Autowired
+    InvestorRepository investorRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(InvestorController.class);
+
+    @RequestMapping(method = RequestMethod.POST, path = "/investors/new")
+    public String saveInvestor(Investors investor){
+        investorRepository.save(investor);
+        return "New Investor! The next Warren Buffett goes by " + investor.getFirstName() + ". All hail this king";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/investor/creation")
+    public String createMostFinanciallyStableMenInWorld(){
+        Investors i1 = new Investors(1, "Warren", "Buffett", "haha@gotyamoney.com", 1234567890, "Bank of America, Apple, Coca-Cola");
+        Investors i2 = new Investors(2, "Elon", "Musk", "emusk@spacetesla.org", 400000000, "Tesla, SpaceX, NASA");
+        Investors i3 = new Investors(3, "Mark", "Zuckerberg", "mark@facebook.com", 989766575, "Facebook, Instagram, YouTube");
+        Investors i4 = new Investors(4, "Nick", "Kokott", "koko@hotmail.com", 987396786, "SpaceX, IBM, Intel");
+        investorRepository.save(i1);
+        investorRepository.save(i2);
+        investorRepository.save(i3);
+        investorRepository.save(i4);
+        return "Created top tier chumps";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/investors")
+    public List<Investors> getAllInvestors(){
+        logger.info("Entered into Controller Layer");
+        List<Investors> results = investorRepository.findAll();
+        logger.info("Number of Investors Found: " + results.size());
+        return results;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/investors/{id}")
+    public Optional<Investors> findInvestorById(@PathVariable("id") int id){
+        logger.info("Entered into Controller Layer");
+        Optional<Investors> results = investorRepository.findById(id);
+        return results;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, path = "/owners/{ownerId}")
+    public Optional<Investors> deleteOwnerById(@PathVariable("id") int id){
+        logger.info("Entered into Controller Layer");
+        Optional<Investors> deleted = investorRepository.findById(id);
+        investorRepository.deleteById(id);
+        return deleted;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, path = "/owners/{ownerId}")
+    public Optional<Investors> updateOwner(@PathVariable("id") int id){
+        investorRepository.getOne(id).setFirstName("Shawty");
+        Optional<Investors> changed = investorRepository.findById(1);
+        return changed;
+    }
+
 }
--- Experiments/kokottni/exp4/tutorials-springboot_unit1_3_petclinic_example/Spring/src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ Experiments/kokottni/exp4/tutorials-springboot_unit1_3_petclinic_example/Spring/src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -11,4 +11,11 @@ class WelcomeController {
     public String welcome() {
         return "Welcome</br> Go to localhost:8080/owners/create to create dummy data </br>";
     }
+
+    @GetMapping("/iamhimothy")
+    public String secretWelcome(){
+        int i = 24;
+        int dinero = i * 10000000;
+        return "You now have all the money in the world hehehe $" + dinero;
+    }
 }
