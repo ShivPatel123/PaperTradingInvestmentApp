ab8a18ded164820403eaec6e14100eed875645c4
Josh <joshw3@iastate.edu>
2023-09-08 16:13:26 -0500

    added an extra button

--- Experiments/joshw3/exp3/tutorials-android_unit1_1_two_screen_counter/two_screen_counter/app/src/main/java/com/example/as1/MainActivity.java
+++ Experiments/joshw3/exp3/tutorials-android_unit1_1_two_screen_counter/two_screen_counter/app/src/main/java/com/example/as1/MainActivity.java
@@ -11,14 +11,26 @@ public class MainActivity extends AppCompatActivity {
 
     Button button;
 
+    Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.toCounterBtn);
+        button2 = findViewById(R.id.toCounterBtn);
 
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, CounterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
