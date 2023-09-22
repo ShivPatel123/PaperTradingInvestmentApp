814692984e3900a4d58c9f57075a8a47f12e4398
Shivansh Patel <shivansh@iastate.edu>
2023-09-21 21:14:20 -0500

    experimenting with h2 db dependencies

---
+++ Experiments/Shivansh/ApiDataTest/src/main/java/io/javabrains/apidatatest/ApiDataTestApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.apidatatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDataTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDataTestApplication.class, args);
+	}
+
+}
