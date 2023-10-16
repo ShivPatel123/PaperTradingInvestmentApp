6561ce8faddca38b39033341c02434fc6d01bba6
kokottni <kokottni@iastate.edu>
2023-10-16 13:52:11 -0500

    Added a Stock Purchased class in order to keep how many stocks and for how much separate

--- Backend/kokottni/Data/src/main/java/coms309/Stocks/StockPurchased.java
+++ Backend/kokottni/Data/src/main/java/coms309/Stocks/StockPurchased.java
@@ -27,6 +27,54 @@ public class StockPurchased {
     @JoinColumn(name = "stock_id")
     private Stock stock;
 
-    @Column
+    @Column(name = "amount_purchased")
+    private int numPurchased;
+
+    @Column(name = "cost_of_purchased")
+    private double costPurchase;
+
+    public StockPurchased(){
+    }
+
+    public User getUser(){return user;}
+
+    public void setUser(User user){this.user = user;}
+
+    public Stock getStock(){return stock;}
+
+    public void setStock(Stock stock){this.stock = stock;}
+
+    public Long getId(){return id;}
+
+    public int getNumPurchased(){return numPurchased;}
+
+    public void setNumPurchased(int numPurchased){this.numPurchased = numPurchased;}
+
+    public double getCostPurchase() {return costPurchase;}
+
+    public void setCostPurchase(double costPurchase){this.costPurchase = costPurchase;}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StockPurchased other = (StockPurchased) obj;
+        if (id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
+
 
 }
