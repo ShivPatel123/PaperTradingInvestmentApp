71306f6ad50ef36c086e049178c9183c9b7cb51b
skutsch <skutsch@iastate.edu>
2023-10-22 13:34:33 -0500

    moved main project file to Frontent/two-screen-counter, rename soon

---
+++ Frontend/two_screen_counter/app/src/main/java/com/example/as1/screens/ProfilePage.java
@@ -0,0 +1,24 @@
+package com.example.as1.screens;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.as1.R;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+
+    }
+
+    
+
+
+
+
+}
