c7f77243b51ad366693d0d47abd4f1980ddace32
skutsch <skutsch@iastate.edu>
2023-10-31 17:04:31 -0500

    websocket started, need to connect to endpoints on backend

--- Frontend/frontend_app/app/src/main/java/com/example/as1/screens/FriendsPage.java
+++ Frontend/frontend_app/app/src/main/java/com/example/as1/screens/FriendsPage.java
@@ -1,9 +1,93 @@
 package com.example.as1.screens;
 
-public class FriendsPage {
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.as1.ExternalControllers.WebSocketListener;
+import com.example.as1.ExternalControllers.WebSocketManager;
+import com.example.as1.R;
+
+import org.java_websocket.handshake.ServerHandshake;
+
+public class FriendsPage extends AppCompatActivity implements WebSocketListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friends_page);
+
+         String BASE_URL = "ws://10.0.2.2:8080/chat/";
+         Button connectBtn, sendBtn;
+         EditText usernameEtx, msgEtx;
+         TextView msgTv;
+
+        connectBtn = findViewById(R.id.bt1);
+        sendBtn =  findViewById(R.id.bt2);
+        usernameEtx =  findViewById(R.id.et1);
+        msgEtx =  findViewById(R.id.et2);
 
 
+        /* connect button listener */
+        connectBtn.setOnClickListener(view -> {
+            String serverUrl = BASE_URL + usernameEtx.getText().toString();
 
+            // Establish WebSocket connection and set listener
+            WebSocketManager.getInstance().connectWebSocket(serverUrl);
+            WebSocketManager.getInstance().setWebSocketListener(FriendsPage.this);
+        });
+
+        /* send button listener */
+        sendBtn.setOnClickListener(v -> {
+            try {
+
+                // send message
+                WebSocketManager.getInstance().sendMessage(msgEtx.getText().toString());
+            } catch (Exception e) {
+                Log.d("ExceptionSendMessage:", e.getMessage().toString());
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onWebSocketMessage(String message) {
+        /**
+         * In Android, all UI-related operations must be performed on the main UI thread
+         * to ensure smooth and responsive user interfaces. The 'runOnUiThread' method
+         * is used to post a runnable to the UI thread's message queue, allowing UI updates
+         * to occur safely from a background or non-UI thread.
+         */
+        TextView msgTv =  findViewById(R.id.tx1);
+        runOnUiThread(() -> {
+            String s = msgTv.getText().toString();
+            msgTv.setText(s + "\n"+message);
+        });
+    }
+
+    @Override
+    public void onWebSocketClose(int code, String reason, boolean remote) {
+        String closedBy = remote ? "server" : "local";
+        TextView msgTv =  findViewById(R.id.tx1);
+        runOnUiThread(() -> {
+            String s = msgTv.getText().toString();
+            msgTv.setText(s + "---\nconnection closed by " + closedBy + "\nreason: " + reason);
+        });
+    }
+
+    @Override
+    public void onWebSocketOpen(ServerHandshake handshakedata) {}
+
+    @Override
+    public void onWebSocketError(Exception ex) {}
 
 
 }
