6d0b6472024609695041579a70afaaa0f38a4ce2
skutsch <skutsch@iastate.edu>
2023-11-06 23:35:59 -0600

    Frontend features merged -- no changes to files

---
+++ Frontend/frontend_app/app/src/main/java/com/example/as1/Fragments/ListView.java
@@ -0,0 +1,16 @@
+package com.example.as1.Fragments;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.as1.R;
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
+
