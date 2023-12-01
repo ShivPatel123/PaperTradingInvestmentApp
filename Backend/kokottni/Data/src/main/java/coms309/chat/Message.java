032fbf2a360c2a8641fc4fdd07aac4c54f76bc81
Shiv Patel <shivanshpatel123@gmail.com>
2023-12-01 00:36:20 -0600

    reached 70% Test Cases

--- Backend/kokottni/Data/src/main/java/coms309/chat/Message.java
+++ Backend/kokottni/Data/src/main/java/coms309/chat/Message.java
@@ -39,11 +39,18 @@ public class Message {
 	
 	public Message() {};
 	
-	public Message(String userName, String content, String target) {
+	public Message(Long id, String userName, String content, String target) {
+        this.id = id;
 		this.userName = userName;
 		this.content = content;
         this.target = target;
 	}
+
+    public Message(String userName, String content, String target) {
+        this.userName = userName;
+        this.content = content;
+        this.target = target;
+    }
 
     public Long getId() {
         return id;
