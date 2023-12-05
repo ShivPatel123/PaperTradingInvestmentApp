2796367bf4c5b4e9d68d48c74a4dda75e09cb45d
Josh <joshw3@iastate.edu>
2023-12-05 16:25:19 -0600

    finalizing changes before editing in main app

--- Frontend/NavViewFE/app/src/main/java/com/example/navviewfe/Screens/StockPageCopyCopyCopy.java
+++ Frontend/NavViewFE/app/src/main/java/com/example/navviewfe/Screens/StockPageCopyCopyCopy.java
@@ -2,18 +2,13 @@ package com.example.navviewfe.Screens;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,17 +17,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.example.navviewfe.Controllers.RecycleViews.StockPreviewScrollAdapter;
-import com.example.navviewfe.Controllers.RecycleViews.StockPreviewScrollCard;
 import com.example.navviewfe.Controllers.Stock;
 import com.example.navviewfe.Controllers.User;
 import com.example.navviewfe.ExternalControllers.VolleySingleton;
@@ -42,21 +30,19 @@ import com.google.android.material.navigation.NavigationView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
 
-public class StockPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class StockPageCopyCopyCopy extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     protected Button newStock_btn, pageLeft, pageRight, buy_btn, sell_btn, delete_btn, toNews_btn;
     protected TextView stockName, stockSymbol, serverNotes;
     protected EditText curr_display;
 
     protected TextView id_display, stockChange;
-    ImageView stockImage, stockLogo;
+    ImageView stockImage;
+
+    ImageView stockStock;
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -69,34 +55,21 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
         setContentView(R.layout.stock_page);
         final int[] index = {0};
 
-        //Set thread permissions to access image url over network
-        int SDK_INT = android.os.Build.VERSION.SDK_INT;
-        if (SDK_INT > 8)
-        {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-                    .permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-
-        }
-
         //get all stocks in an array
         ArrayList<Stock> stockArrayList = new ArrayList<>();
         stockArrayList = getAllStocks(this.getApplicationContext());
-        Log.i("getAllStocks req", "stocklist array: " + stockArrayList.toString());
-
-        //set first image
-        getStockLogo(this.getApplicationContext(), 1);
+        Log.i("StockPage getAllStocks after req", "stock array list for screen: " + stockArrayList.toString());
 
         //Side nav bar
         drawerLayout = findViewById(R.id.drawer_layout_stock);
         navigationView = findViewById(R.id.nav_view_stock);
         menu = navigationView.getMenu();
 //        menu.findItem(R.id.nav_logout).setVisible(false);
+        menu.findItem(R.id.nav_login).setVisible(false);
+
 //        menu.findItem(R.id.nav_profile).setVisible(false);
 //        menu.findItem(R.id.nav_group).setVisible(false);
 //        menu.findItem(R.id.nav_group_edit).setVisible(false);
-        menu.findItem(R.id.nav_login).setVisible(false);
-
 
         navigationView.bringToFront();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -129,7 +102,7 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
                 serverNotes.setText("Only Admins can create Stocks");
             }
             else {
-                Intent intent = new Intent(StockPage.this, CreateStock.class);
+                Intent intent = new Intent(StockPageCopyCopyCopy.this, CreateStock.class);
                 startActivity(intent);
             }
         });
@@ -137,80 +110,85 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
         //View News Button
         toNews_btn = findViewById(R.id.toNews_btn);
         toNews_btn.setOnClickListener(view -> {
-            Intent newsIntent = new Intent(StockPage.this, NewsPage.class);
+            Intent newsIntent = new Intent(StockPageCopyCopyCopy.this, NewsPage.class);
             //+ 1 so index matches stock id
             index[0] += 1;
             newsIntent.putExtra("index", index[0]);
             startActivity(newsIntent);
         });
 
-//        //Buy Button
-//        buy_btn = findViewById(R.id.buy_StockPagebtn);
-//        ArrayList<Stock> finalStockArrayList = stockArrayList;
-//        buy_btn.setOnClickListener(view -> {
-//            Stock object;
-//            object = finalStockArrayList.get(index[0]);
-//            long id = object.getId();
-//            BuyStock(this.getApplicationContext(), getGlobal, (int) id);
-//        });
-//
-//        //Sell Button
-//        sell_btn = findViewById(R.id.sell_StockPagebtn);
-//        ArrayList<Stock> finalStockArrayList4 = stockArrayList;
-//        sell_btn.setOnClickListener(view -> {
-//            Stock object;
-//            object = finalStockArrayList4.get(index[0]);
-//            long id2 = object.getId();
-//            SellStock(this.getApplicationContext(), getGlobal, (int) id2);
-//        });
+        //Buy Button
+        buy_btn = findViewById(R.id.buy_StockPagebtn);
+        ArrayList<Stock> finalStockArrayList = stockArrayList;
+        buy_btn.setOnClickListener(view -> {
+            Stock object;
+            object = finalStockArrayList.get(index[0]);
+            long id = object.getId();
+            BuyStock(this.getApplicationContext(), getGlobal, (int) id);
+        });
+
+        //Sell Button
+        sell_btn = findViewById(R.id.sell_StockPagebtn);
+        ArrayList<Stock> finalStockArrayList4 = stockArrayList;
+        sell_btn.setOnClickListener(view -> {
+            Stock object;
+            object = finalStockArrayList4.get(index[0]);
+            long id2 = object.getId();
+            SellStock(this.getApplicationContext(), getGlobal, (int) id2);
+        });
 
         //History Button
         Button toHistory_btn = findViewById(R.id.toHistory_btn);
         toHistory_btn.setOnClickListener(view -> {
-            Intent historyIntent = new Intent(StockPage.this, HistoryPage.class);
+            Intent historyIntent = new Intent(StockPageCopyCopyCopy.this, HistoryPage.class);
             //+ 1 so index matches stock id
             index[0] += 1;
             historyIntent.putExtra("index", index[0]);
             startActivity(historyIntent);
         });
 
-//        //Delete Stock Button
-//        delete_btn = findViewById(R.id.delete_StockPagebtn);
-//        serverNotes = findViewById(R.id.notesEditText);
-//        id_display = findViewById(R.id.stockID_view);
-//        curr_display  = findViewById(R.id.stockPrice_Display);
-//        ArrayList<Stock> finalStockArrayList3 = stockArrayList;
-//        newStock_btn.setOnClickListener(view -> {
-//            if(getGlobal.getPrivilege() != 'a'){
-//                serverNotes.setText("Only Admins can delete Stocks");
-//            }
-//            else {
-//                //delete stock
-//                Stock object;
-//                object = finalStockArrayList3.get(index[0]);
-//                long id3 = object.getId();
-//                deleteStock(this.getApplicationContext(), getGlobal, (int) id3);
-//                //remove from arraylist
-//                finalStockArrayList3.remove(index[0]);
-//                index[0] -= 1;
-//                //reset view to stock at previous index
-//                object = finalStockArrayList3.get(index[0]);
-//                stockName.setText(object.getCompany());
-//                stockSymbol.setText(object.getSymbol());
-//                id_display.setText("ID: " + Math.toIntExact(object.getId()));
-//                curr_display.setText("" + (int) object.getCurrValue());
-//
-//                double stockPercentNum = (object.getPrevDayChange() / object.getCurrValue()) * 100;
-//                String stockPercent = String.format("%.3f", stockPercentNum);
-//                stockChange.setText("" + object.getPrevDayChange() + " || " + stockPercent + "%");
-//                if(object.getPrevDayChange() < 0){
-//                    stockImage.setImageResource(R.drawable.stockredarrow);
-//                }
-//                else{
-//                    stockImage.setImageResource(R.drawable.greenarrow);
-//                }
-//            }
-//        });
+        //Delete Stock Button
+        delete_btn = findViewById(R.id.delete_StockPagebtn);
+        serverNotes = findViewById(R.id.notesEditText);
+        id_display = findViewById(R.id.stockID_view);
+        curr_display  = findViewById(R.id.stockPrice_Display);
+
+        stockStock = findViewById(R.id.stockImageImageView);
+
+        ArrayList<Stock> finalStockArrayList3 = stockArrayList;
+        newStock_btn.setOnClickListener(view -> {
+            if(getGlobal.getPrivilege() != 'a'){
+                serverNotes.setText("Only Admins can delete Stocks");
+            }
+            else {
+                //delete stock
+                Stock object;
+                object = finalStockArrayList3.get(index[0]);
+                long id3 = object.getId();
+                deleteStock(this.getApplicationContext(), getGlobal, (int) id3);
+                //remove from arraylist
+                finalStockArrayList3.remove(index[0]);
+                index[0] -= 1;
+                //reset view to stock at previous index
+                object = finalStockArrayList3.get(index[0]);
+                stockName.setText(object.getCompany());
+                stockSymbol.setText(object.getSymbol());
+                id_display.setText("ID: " + Math.toIntExact(object.getId()));
+                curr_display.setText("" + (int) object.getCurrValue());
+
+                double stockPercentNum = (object.getPrevDayChange() / object.getCurrValue()) * 100;
+                String stockPercent = String.format("%.3f", stockPercentNum);
+                stockChange.setText("" + object.getPrevDayChange() + " || " + stockPercent + "%");
+                if(object.getPrevDayChange() < 0){
+                    stockImage.setImageResource(R.drawable.stockredarrow);
+                    stockStock.setImageResource(R.drawable.stockredarrow);
+                }
+                else{
+                    stockImage.setImageResource(R.drawable.greenarrow);
+                    stockStock.setImageResource(R.drawable.greenarrow);
+                }
+            }
+        });
 
         //Page Left Button
         pageLeft = findViewById(R.id.prev_StockPageBtn);
@@ -240,16 +218,18 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
                 id_display.setText("ID: " + Math.toIntExact(object.getId()));
                 curr_display.setText("" + (int) object.getCurrValue());
 
-                getStockLogo(this.getApplicationContext(), Math.toIntExact(object.getId()));
-
                 double stockPercentNum = (object.getPrevDayChange() / object.getCurrValue()) * 100;
                 String stockPercent = String.format("%.3f", stockPercentNum);
                 stockChange.setText("" + object.getPrevDayChange() + " || " + stockPercent + "%");
                 if(object.getPrevDayChange() < 0){
                     stockImage.setImageResource(R.drawable.stockredarrow);
+                    stockStock.setImageResource(R.drawable.stockredarrow);
+
                 }
                 else{
                     stockImage.setImageResource(R.drawable.greenarrow);
+                    stockStock.setImageResource(R.drawable.greenarrow);
+
                 }
             }
         });
@@ -279,56 +259,22 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
                 id_display.setText("ID: " + Math.toIntExact(object.getId()));
                 curr_display.setText("" + (int) object.getCurrValue());
 
-                getStockLogo(this.getApplicationContext(), Math.toIntExact(object.getId()));
-
                 double stockPercentNum = (object.getPrevDayChange() / object.getCurrValue()) * 100;
                 String stockPercent = String.format("%.3f", stockPercentNum);
                 stockChange.setText("" + object.getPrevDayChange() + " || " + stockPercent + "%");
                 if(object.getPrevDayChange() < 0){
                     stockImage.setImageResource(R.drawable.stockredarrow);
+                    stockStock.setImageResource(R.drawable.stockredarrow);
+
                 }
                 else{
                     stockImage.setImageResource(R.drawable.greenarrow);
+                    stockStock.setImageResource(R.drawable.greenarrow);
+
                 }
             }
         });
     }
-    public void getStockLogo(Context context, int id) {
-        ImageView stockLogo = findViewById(R.id.stockImageImageView);
-        String URL_JSON_OBJECT = "http://10.90.75.130:8080/stocks/" + id +"/logo";
-        //Create new request
-        JsonObjectRequest request = new JsonObjectRequest(
-                Request.Method.GET,
-                URL_JSON_OBJECT,
-                null,
-                response -> {
-                    Log.i(" full response", "getStockLogo: " + response);
-                    //set image
-                    try {
-                        String url = response.getString("logo");
-                        InputStream URLcontent = null;
-                        try {
-                            URLcontent = (InputStream) new URL(url).getContent();
-                        }catch (MalformedURLException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                        Drawable image = Drawable.createFromStream(URLcontent, "");
-                        stockLogo.setImageDrawable(image);
-
-                    } catch (JSONException e) {
-                        throw new RuntimeException(e);
-                    }
-                },
-
-                error -> Log.i("Error ", "getStockLogo: " + error.getMessage())) {
-        };
-
-        // Adding request to request queue
-        VolleySingleton.getInstance(context.getApplicationContext()).addToRequestQueue(request);
-    }
-
 
     public ArrayList<Stock> getAllStocks(Context context) {
         String URL_JSON_OBJECT = "http://10.90.75.130:8080/stocks";
@@ -339,6 +285,7 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
         curr_display  = findViewById(R.id.stockPrice_Display);
         stockChange = findViewById(R.id.stockChange_txt);
         stockImage = findViewById(R.id.stock_ImageView);
+        stockStock = findViewById(R.id.stockImageImageView);
 
         //Create new request
         JsonArrayRequest request = new JsonArrayRequest(
@@ -377,9 +324,13 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
                     stockChange.setText("" + stockObject.getPrevDayChange() + " || " +  stockPercent + "%");
                     if(stockObject.getPrevDayChange() < 0){
                         stockImage.setImageResource(R.drawable.stockredarrow);
+                        stockStock.setImageResource(R.drawable.stockredarrow);
+
                     }
                     else{
                         stockImage.setImageResource(R.drawable.greenarrow);
+                        stockStock.setImageResource(R.drawable.greenarrow);
+
                     }
                 },
 
@@ -458,28 +409,28 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         if (menuItem.getItemId() == R.id.nav_home){
-            Intent intent = new Intent(StockPage.this, LoggedInPage.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, LoggedInPage.class);
             startActivity(intent);
         } else if (menuItem.getItemId() == R.id.nav_stock){
-            Intent intent = new Intent(StockPage.this, StockPageCopyCopyCopy.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, StockPageCopyCopyCopy.class);
             startActivity(intent);
         } else if (menuItem.getItemId() == R.id.nav_stock_list) {
-            Intent intent = new Intent(StockPage.this, StockList.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, StockList.class);
             startActivity(intent);
         } else if (menuItem.getItemId() == R.id.nav_login) {
-            Intent intent = new Intent(StockPage.this, StartPage.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, StartPage.class);
             startActivity(intent);
         } else if (menuItem.getItemId() == R.id.nav_group_edit) {
-            Intent intent = new Intent(StockPage.this, AdminDashboardPage.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, AdminDashboardPage.class);
             startActivity(intent);
         } else if (menuItem.getItemId() == R.id.nav_group) {
-            Intent intent = new Intent(StockPage.this, GroupPage.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, GroupPage.class);
             startActivity(intent);
         } else if (menuItem.getItemId() == R.id.nav_logout) {
-            Intent intent = new Intent(StockPage.this, StartPage.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, MainActivity.class);
             startActivity(intent);
         } else if (menuItem.getItemId() == R.id.nav_profile) {
-            Intent intent = new Intent(StockPage.this, ProfilePage.class);
+            Intent intent = new Intent(StockPageCopyCopyCopy.this, ProfilePage.class);
             startActivity(intent);
         }
 
@@ -493,3 +444,4 @@ public class StockPage extends AppCompatActivity implements NavigationView.OnNav
         super.onPointerCaptureChanged(hasCapture);
     }
 }
+
